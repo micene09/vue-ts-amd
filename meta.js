a08a2be36fc4814abf68ce8fbf46584a7d47dc33
Micene <giovanni.contino09@gmail.com>
2017-12-27 02:52:42 +0100

    Fixing installation file, first try

--- meta.js
+++ meta.js
@@ -1,7 +1,7 @@
 module.exports = {
+	skipInterpolation: "src/**/*.*",
 	completeMessage: "To get started:\n\n"
 		+ "cd {{destDirName}}\n"
 		+ "npm install\n"
-		+ "unzip the example_project.zip (overwrite src and test folder)\n"
 		+ "npm run dev"
 };
