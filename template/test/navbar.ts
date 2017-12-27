bee751c3c526e3ef7ea126cda79e01ade999ccf0
Micene <giovanni.contino09@gmail.com>
2017-12-27 14:13:54 +0100

    Fixed tests codebase

--- template/test/navbar.ts
+++ template/test/navbar.ts
@@ -1,10 +1,16 @@
-import { expect } from "chai";
-import { mount } from "avoriaz";
-import Navbar from '../src/components/navbar/navbar';
+import { expect as ChaiExpect } from "chai";
+import { mount as AvoriazMount } from "avoriaz";
 
-describe('Navbar component', () => {
-	let wrapper = mount(Navbar);
-	it('renders 2 links', () => {
-		expect(wrapper.find('.navbar-nav > li')).to.have.lengthOf(2);
+define(["chai", "avoriaz", "../src/components/navbar/navbar"],
+(Chai: any, Avoriaz: any, Navbar: any) => {
+
+	let mount: typeof AvoriazMount = Avoriaz.mount,
+		expect: typeof ChaiExpect = Chai.expect;
+
+	describe('Navbar component', () => {
+		let wrapper = mount(Navbar);
+		it('renders 2 links', () => {
+			expect(wrapper.find('.navbar-nav > li')).to.have.lengthOf(2);
+		});
 	});
 });
--- template/test/test-example.ts
+++ template/test/test-example.ts
@@ -1,11 +1,17 @@
-import * as Chai from "chai";
-var expect = Chai.expect;
+import { expect as ChaiExpect } from "chai";
 
-describe('Some fake tests', function () {
-	it('a fake test that should pass...', function () {
-		expect("1").to.be.equal("1");
-	});
-	it('a fake test that will fail...', function () {
-		expect(true).to.be.false;
+define(["chai"], (Chai: any) => {
+
+	let expect: typeof ChaiExpect = Chai.expect;
+
+	describe('Some fake tests', function () {
+
+		it('a fake test that should pass...', function () {
+			expect("1").to.be.equal("1");
+		});
+		it('a fake test that will fail...', function () {
+			expect(true).to.be.false;
+		});
 	});
 });
+
