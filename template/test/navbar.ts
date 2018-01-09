26a1866037389117181ac0a6e463e463bccd898a
Micene <giovanni.contino09@gmail.com>
2018-01-09 23:35:34 +0100

    Arrow functions used now

--- template/test/navbar.ts
+++ template/test/navbar.ts
@@ -2,9 +2,9 @@ import { expect } from "chai";
 import { mount } from "avoriaz";
 import Navbar from "../src/components/navbar/navbar";
 
-describe('Navbar component', function() {
+describe('Navbar component', () => {
 	let wrapper = mount(Navbar);
-	it('renders 2 links', function() {
+	it('renders 2 links', () => {
 		expect(wrapper.find('.navbar-nav > li')).to.have.lengthOf(2);
 	});
 });
--- template/test/test-example.ts
+++ template/test/test-example.ts
@@ -1,11 +1,11 @@
 import { expect } from "chai";
 
-describe('Some fake tests', function () {
+describe('Some fake tests', () => {
 
-	it('a fake test that should pass...', function () {
+	it('a fake test that should pass...', () => {
 		expect("1").to.be.equal("1");
 	});
-	it('a fake test that will fail...', function () {
+	it('a fake test that will fail...', () => {
 		expect(true).to.be.false;
 	});
 });
